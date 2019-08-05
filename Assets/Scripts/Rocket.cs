5ccd275c1ca33bf485ab1f910de342fbf10abc9b
BrendorTheEndor <brenden_hoverxd@live.com>
2019-08-05 14:16:12 -0500

    Better movement and some level design

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -19,12 +19,32 @@ public class Rocket : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        ProcessInput();
+        Rotate();
+    }
+
+    private void FixedUpdate() {
+        Thrust();
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        switch(collision.gameObject.tag) {
+            case "Friendly":
+                Debug.Log("Fine");
+                break;
+            default:
+                Debug.Log("Dead");
+                break;
+        }
     }
 
     private void ProcessInput() {
+        Thrust();
+        Rotate();
+    }
+
+    private void Thrust() {
         if(Input.GetKey(KeyCode.Space)) {
-            myRigidbody.AddRelativeForce(new Vector3(0f, thrustSpeed * Time.deltaTime, 0f));
+            myRigidbody.AddRelativeForce(new Vector3(0f, thrustSpeed, 0f)); // functionally same as vector3.up, no time.deltatime because it's a physics calculation
             if(!myAudioSource.isPlaying) {
                 myAudioSource.Play();
             }
@@ -32,6 +52,11 @@ public class Rocket : MonoBehaviour {
         else {
             myAudioSource.Stop();
         }
+    }
+
+    private void Rotate() {
+
+        myRigidbody.freezeRotation = true; // Take manual control of rotation
 
         if(Input.GetKey(KeyCode.D)) {
             transform.Rotate(new Vector3(0f, 0f, -rotationSpeed * Time.deltaTime));
@@ -39,5 +64,7 @@ public class Rocket : MonoBehaviour {
         else if(Input.GetKey(KeyCode.A)) {
             transform.Rotate(new Vector3(0f, 0f, rotationSpeed * Time.deltaTime));
         }
+
+        myRigidbody.freezeRotation = false; // Resume standard rotation (physics controlled)
     }
 }
