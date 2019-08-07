efc7bd4728ac4dc55d2f79fc161e4d83a6c06f91
BrendorTheEndor <brenden_hoverxd@live.com>
2019-08-07 16:55:25 -0500

    Pre lighting settings crash fix

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+
+    [Range(0, 1)] [SerializeField] float movementFactor; // Serialized for debug, basically the % of oscillation completed
+    [SerializeField] Vector3 movementVector; // the end offset of the desired transform (oscillates to this offset)
+    [SerializeField] float period; // Our desired period of oscillation
+
+    Vector3 startingPos;
+
+    // Start is called before the first frame update
+    void Start() {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if(period <= Mathf.Epsilon) { return; }
+
+        float cycles = Time.time / period; // Grows continuously from 0, how many cycles we've completed so far
+        const float tau = Mathf.PI * 2f; // About 6.28, just a constant
+        float rawSinWave = Mathf.Sin(cycles * tau); // Varies from -1 to 1, basically getting a sin wave by converting cycles to radians
+        movementFactor = (rawSinWave / 2f) + .5f; // Goes from 0 to 1, just manipulates the sin wave output to be from 0 to 1
+
+        Vector3 offset = movementVector * movementFactor; // Calculates the actual offset to place the object at
+        transform.position = startingPos + offset; // Moves the object
+    }
+}
--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -2,11 +2,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
 
     [SerializeField] float thrustSpeed = 1f;
     [SerializeField] float rotationSpeed = 1f;
+    [SerializeField] float timeToDelayLevelLoad = 1f;
+
+    [SerializeField] AudioClip thrustSFX;
+    [SerializeField] AudioClip deathSFX;
+    [SerializeField] AudioClip levelCompleteSFX;
+
+    [SerializeField] ParticleSystem thrustVFX;
+    [SerializeField] ParticleSystem deathVFX;
+    [SerializeField] ParticleSystem levelCompleteVFX;
+
+    enum State { Alive, Dying, Transcending }
+    State currentState = State.Alive;
 
     Rigidbody myRigidbody;
     AudioSource myAudioSource;
@@ -19,21 +32,51 @@ public class Rocket : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        Rotate();
+        if(currentState == State.Alive) {
+            Rotate();
+        }
     }
 
     private void FixedUpdate() {
-        Thrust();
+        if(currentState == State.Alive) {
+            Thrust();
+        }
     }
 
     private void OnCollisionEnter(Collision collision) {
+
+        if(currentState != State.Alive) { return; }
+
         switch(collision.gameObject.tag) {
             case "Friendly":
-                Debug.Log("Fine");
+                break;
+            case "Finish":
+                currentState = State.Transcending;
+                myAudioSource.Stop();
+                myAudioSource.PlayOneShot(levelCompleteSFX);
+                levelCompleteVFX.Play();
+                Invoke("LoadNextScene", timeToDelayLevelLoad);
                 break;
             default:
-                Debug.Log("Dead");
+                currentState = State.Dying;
+                myAudioSource.Stop();
+                myAudioSource.PlayOneShot(deathSFX);
+                deathVFX.Play();
+                Invoke("ReloadLevel", timeToDelayLevelLoad);
                 break;
+        }
+    }
+
+    private void ReloadLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void LoadNextScene() {
+        if(SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCountInBuildSettings - 1) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else {
+            SceneManager.LoadScene(0);
         }
     }
 
@@ -46,11 +89,13 @@ public class Rocket : MonoBehaviour {
         if(Input.GetKey(KeyCode.Space)) {
             myRigidbody.AddRelativeForce(new Vector3(0f, thrustSpeed, 0f)); // functionally same as vector3.up, no time.deltatime because it's a physics calculation
             if(!myAudioSource.isPlaying) {
-                myAudioSource.Play();
+                myAudioSource.PlayOneShot(thrustSFX);
             }
+            thrustVFX.Play();
         }
         else {
             myAudioSource.Stop();
+            thrustVFX.Stop();
         }
     }
 
